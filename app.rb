695d4bd1321a06de7341c1187f0a8e44e703d120
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-08-05 03:21:45 +0000

    Cleaned up a bit

--- app.rb
+++ app.rb
@@ -2,29 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "json"
 require 'http'
-symbols_url = "https://api.exchangerate.host/symbols"
 amounts_url = "https://api.exchangerate.host/latest"
 response_amounts = HTTP.get(amounts_url)
-response_symbols = HTTP.get(symbols_url)
 response_amounts_array = JSON.parse(response_amounts)
-response_symbols_array = JSON.parse(response_symbols)
 @levelone = response_amounts_array.fetch("rates")
-
-
-
 
 get("/") do
   @levelone = response_amounts_array.fetch("rates")
   erb(:home)
 end
 
-
-
-
 get("/:symbol") do
   @symbol = params[:symbol]
   @levelone = response_amounts_array.fetch("rates")
-  @url_symbol = params[:symbol]
   erb(:symbol)
 end
 
@@ -32,6 +22,5 @@ get("/:symbol/:symbol2") do
   @symbol = params[:symbol]
   @symbol_two = params[:symbol2]
   @levelone = response_amounts_array.fetch("rates")
-  @url_symbol = params[:symbol2]
   erb(:symbol_two)
 end
