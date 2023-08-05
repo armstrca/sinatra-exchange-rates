156d834c17004c6f74cfffcca1a1597bddbb6d74
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-08-05 03:36:51 +0000

    erroneously called some hashes arrays, fixed that

--- app.rb
+++ app.rb
@@ -4,23 +4,27 @@ require "json"
 require 'http'
 amounts_url = "https://api.exchangerate.host/latest"
 response_amounts = HTTP.get(amounts_url)
-response_amounts_array = JSON.parse(response_amounts)
-@levelone = response_amounts_array.fetch("rates")
+response_amounts_hash = JSON.parse(response_amounts)
+@levelone = response_amounts_hash.fetch("rates")
+
+pp response_amounts_hash
 
 get("/") do
-  @levelone = response_amounts_array.fetch("rates")
+  @levelone = response_amounts_hash.fetch("rates")
   erb(:home)
 end
 
 get("/:symbol") do
   @symbol = params[:symbol]
-  @levelone = response_amounts_array.fetch("rates")
+  @levelone = response_amounts_hash.fetch("rates")
+  @url_symbol = params[:symbol]
   erb(:symbol)
 end
 
 get("/:symbol/:symbol2") do
   @symbol = params[:symbol]
   @symbol_two = params[:symbol2]
-  @levelone = response_amounts_array.fetch("rates")
+  @levelone = response_amounts_hash.fetch("rates")
+  @url_symbol = params[:symbol2]
   erb(:symbol_two)
 end
